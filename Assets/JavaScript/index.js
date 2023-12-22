7186da4c6bbac5f9be3eb15532680546e678c078
unknown <ea0094695@gmail.com>
2023-12-21 22:32:04 -0300

    Atualizacao 0.2

---
+++ Assets/JavaScript/index.js
@@ -0,0 +1,30 @@
+const btnTecnologias = document.getElementById("btn-tecnologias");
+
+const textCard = document.getElementById("text-card");
+
+btnTecnologias.addEventListener("click", () => {
+  textCard.innerHTML = `
+    <p>As tecnologias que utilizo atualmente são:</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>Bootstrap</li>
+                    <li>ReactJs</li>
+                    <li>Node.js</li>
+                    <li>MySQL</li>
+                </ul>
+                <button class="tecnologias" id="btn-voltar">Voltar</button>
+
+
+`;
+  const btnVoltar = document.getElementById("btn-voltar");
+  btnVoltar.addEventListener("click", () => {
+    textCard.innerHTML = `
+                <p>Me chamo Eduardo , sou desenvolvedor web, com experiência em projetos que vão desde sistemas Kanban até bancos digitais e plataformas de venda de automóveis.</p>
+                <p> Comprometido com a excelência, estou sempre em busca de desafios para aprimorar minhas habilidades e contribuir com soluções inovadoras.</p>
+                <button class="tecnologias" id="btn-tecnologias">Tecnologias</button>
+
+`;
+  });
+});
